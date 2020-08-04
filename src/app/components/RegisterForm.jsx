3eaeb5e0dc55a327c5a38190d7f72b10bb541dee
Vladimir Denisov <vdenisov.dev@gmail.com>
2020-08-04 05:05:06 +0300

    [05-using-forms] 2. handlers "submit form" and "input change"

--- src/app/components/RegisterForm.jsx
+++ src/app/components/RegisterForm.jsx
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
 
 class RegisterForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { email: '' };
+    }
+
+    handleSubmit() {
+        console.log('form is submitted');
+    }
+
+    handleEmailChange() {
+        console.log('email was changed');
+    }
+
     render() {
         return (
-            <form>
-                <input type="text" placeholder="E-mail" />
+            <form onSubmit={this.handleSubmit.bind(this)}>
+                <input type="text" placeholder="E-mail" value={this.state.email} onChange={this.handleEmailChange.bind(this)} />
             </form>
         );
     }

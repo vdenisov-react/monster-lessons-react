5b816b9669e3f18bdcacb3d577b271695497e7ff
Vladimir Denisov <vdenisov.dev@gmail.com>
2020-08-04 07:57:05 +0300

    * remove all changes for application

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,37 +1,14 @@
-import React from 'react';
-import { Router, Route } from 'react-router';
-import { createBrowserHistory } from 'history';
-
-import Header from './components/Header';
-import Home from './pages/Home';
-import About from './pages/About';
-
+import React, { Component } from 'react';
 import './App.css';
 
-const hashHistory = createBrowserHistory();
-
-const menu = [
-    {
-        link: '/',
-        label: 'Home',
-    },
-    {
-        link: '/about',
-        label: 'About',
-    },
-];
-
-const App = () => {
-    return (
-        <div>
-            <Header items={menu} />
-
-            <Router history={hashHistory}>
-                <Route exact path="/" component={Home} />
-                <Route path="/about" component={About} />
-            </Router>
-        </div>
-    );
-};
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <div>Our App</div>
+            </div>
+        );
+    }
+}
 
 export default App;
--- src/app/pages/About.jsx
+++
@@ -1,7 +0,0 @@
-import React, { Component } from 'react';
-
-const About = () => {
-    return <div>ABOUT page</div>;
-};
-
-export default About;

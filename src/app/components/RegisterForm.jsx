30907f4f6250567ecdf4f8945ed7674dfeb48534
Vladimir Denisov <vdenisov.dev@gmail.com>
2020-08-04 05:48:35 +0300

    [05-using-forms] 4. update internal logic for handlers

--- src/app/components/RegisterForm.jsx
+++ src/app/components/RegisterForm.jsx
@@ -8,12 +8,14 @@ class RegisterForm extends Component {
         this.handleEmailChange = this.handleEmailChange.bind(this);
     }
 
-    handleSubmit() {
-        console.log('form is submitted');
+    handleSubmit(event) {
+        event.preventDefault();
+        console.log('Form is submitted. Email value is =>', this.state.email);
     }
 
-    handleEmailChange() {
-        console.log('email was changed');
+    handleEmailChange(event) {
+        console.log('Email was changed =>', event.target.value);
+        this.setState({ email: event.target.value });
     }
 
     render() {

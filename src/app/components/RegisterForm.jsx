3223d1be2f9cb5124223a2605a1aef7aca51556e
Vladimir Denisov <vdenisov.dev@gmail.com>
2020-08-04 05:00:53 +0300

    [05-using-forms] 1. add simple component with form

---
+++ src/app/components/RegisterForm.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class RegisterForm extends Component {
+    render() {
+        return (
+            <form>
+                <input type="text" placeholder="E-mail" />
+            </form>
+        );
+    }
+}
+
+export default RegisterForm;

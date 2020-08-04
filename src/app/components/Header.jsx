88a48c8b3a3da7a3fc31e4a41b46470d8e443327
Vladimir Denisov <vdenisov.dev@gmail.com>
2020-08-04 04:30:24 +0300

    [03-using-props] 4. render "menu" items in "Header"

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -3,7 +3,15 @@ import React, { Component } from 'react';
 class Header extends Component {
     render() {
         console.log('items =>', this.props.items);
-        return <div>Header</div>;
+        return (
+            <ul>
+                {this.props.items.map((item, index) => (
+                    <li key={index}>
+                        <a href={item.link}>{item.label}</a>
+                    </li>
+                ))}
+            </ul>
+        );
     }
 }
 

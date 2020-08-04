22bdb0079a9d0f2d2c67709e66a35bfce79b74bc
Vladimir Denisov <vdenisov.dev@gmail.com>
2020-08-04 06:35:32 +0300

    [07-using-refs] 3. use DOM element via "ref"

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -3,13 +3,13 @@ import './App.css';
 
 class App extends Component {
     submit() {
-        console.log('submit');
+        console.log('submit =>', this.testInput.value);
     }
 
     render() {
         return (
             <div>
-                <input type="text" placeholder="text" />
+                <input type="text" placeholder="text" ref={input => (this.testInput = input)} />
                 <button onClick={this.submit.bind(this)}>Submit</button>
             </div>
         );

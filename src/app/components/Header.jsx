dc670e9c5393057968b3c6998beb691c9c79caf4
Vladimir Denisov <vdenisov.dev@gmail.com>
2020-08-04 04:42:25 +0300

    [04-prop-types] 1. add validation for prop "items"

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class Header extends Component {
+    static propTypes = {
+        items: PropTypes.array.isRequired,
+    };
+
     render() {
         console.log('items =>', this.props.items);
         return (

807201e7d45db9d5ca73d4c21785e1bb47783430
Vladimir Denisov <vdenisov.dev@gmail.com>
2020-08-04 04:25:14 +0300

    [03-using-props] 3. pass "menu" to "Header" component

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -20,7 +20,7 @@ class App extends Component {
     render() {
         return (
             <div>
-                <Header />
+                <Header items={menu} />
             </div>
         );
     }
--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 class Header extends Component {
     render() {
+        console.log('items =>', this.props.items);
         return <div>Header</div>;
     }
 }

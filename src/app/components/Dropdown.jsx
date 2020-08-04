4f57b138c098a3bafbf41375306f49d9d65de69e
Vladimir Denisov <vdenisov.dev@gmail.com>
2020-08-04 03:29:28 +0300

    [02-local-state] 2. add variable to component state

--- src/app/components/Dropdown.jsx
+++ src/app/components/Dropdown.jsx
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
 
 class Dropdown extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isOpened: false,
+        };
+    }
+
     render() {
         return <div>Its dropdown baby</div>;
     }

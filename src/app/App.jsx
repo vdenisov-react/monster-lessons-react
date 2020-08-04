058a5f737ebb634c9307f91e391bf26c00f68256
Vladimir Denisov <vdenisov.dev@gmail.com>
2020-08-04 07:17:42 +0300

    [08-react-router] 4. add navigation menu for routing

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Router, Route } from 'react-router';
 import { createBrowserHistory } from 'history';
 
+import Header from './components/Header';
 import Home from './pages/Home';
 import About from './pages/About';
 
@@ -9,13 +10,28 @@ import './App.css';
 
 const hashHistory = createBrowserHistory();
 
+const menu = [
+    {
+        link: '/',
+        label: 'Home',
+    },
+    {
+        link: '/about',
+        label: 'About',
+    },
+];
+
 class App extends Component {
     render() {
         return (
-            <Router history={hashHistory}>
-                <Route exact path="/" component={Home} />
-                <Route path="/about" component={About} />
-            </Router>
+            <div>
+                <Header items={menu} />
+
+                <Router history={hashHistory}>
+                    <Route exact path="/" component={Home} />
+                    <Route path="/about" component={About} />
+                </Router>
+            </div>
         );
     }
 }
